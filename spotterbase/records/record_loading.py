581420a5ffe62f68ab68b48d38909a3161977638
jfschaefer <jan.frederik.schaefer@fau.de>
2023-04-17 20:42:51 +0200

    support loading referenced records from SPARQL endpoint

--- spotterbase/records/record_loading.py
+++ spotterbase/records/record_loading.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Iterable
 
 from spotterbase.records.record import Record
 from spotterbase.records.sparql_populate import Populator
@@ -21,3 +21,38 @@ SELECT DISTINCT ?uri WHERE {{
             yield uri
 
     yield from populator.get_records(uris=uri_iterator())
+
+
+def _get_uris_from_record_vals(vals: list) -> Iterator[Uri]:
+    for val in vals:
+        if isinstance(val, Uri):
+            yield val
+        elif isinstance(val, Record):
+            yield from _get_uris_from_record(val)
+
+
+def _get_uris_from_record(record: Record) -> Iterator[Uri]:
+    for attr in record.record_info.attrs:
+        if not hasattr(record, attr.attr_name):
+            continue
+        val = getattr(record, attr.attr_name)
+        if isinstance(val, list):
+            yield from _get_uris_from_record_vals(val)
+        else:
+            yield from _get_uris_from_record_vals([val])
+
+
+def load_all_records_transitively(uris: Iterable[Uri], populator: Populator) -> Iterator[Record]:
+    processed_uris: set[Uri] = set()
+    unprocessed_uris: set[Uri] = set(uris)
+
+    while unprocessed_uris:
+        new_records = list(populator.get_records(uris=unprocessed_uris))
+        processed_uris.update(unprocessed_uris)
+        unprocessed_uris = set()
+        for record in new_records:
+            yield record
+
+            for uri in _get_uris_from_record(record):
+                if uri not in processed_uris:
+                    unprocessed_uris.add(uri)
